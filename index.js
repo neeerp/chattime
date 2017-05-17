eee1546faada3d8f0248d6d29d546a7c50cd4f24
David Goldstein <davey_wavey@rocketmail.com>
2017-05-16 20:07:01 -0400

    Text bubbles now receive tags to indicate whether they're a part of a block of text bubbles (To be used for styling purposes)

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ $(document).ready(function() {
 
 	$("textarea").keydown(function(event) {
 		if (event.keyCode == 13 && !event.shiftKey) {
-
-			var client = $(this).parents(".left").length != 0 ? "left" : "right";
 			var message = $(this).val();
-			$(".message-area").html($(".message-area").html() + "<p class=\"" + client + "\">" + message + "</p>");
-
+			if (message !== "") {		
+				var client = $(this).parents(".left").length != 0 ? "left" : "right";
+				$(".message-area").html(
+					$(".message-area").html() + "<p class=\"" + client + "\">" + message + "</p>");
+				$(".message-area").scrollTop($(".message-area").prop("scrollHeight")); /** Scroll messages to bottom **/
+				$(".message-area").each( function() {orientTextBoxes(this)});
+			}
 
 			$(this).val(""); /** Clear the text area **/
 			event.preventDefault();
@@ -16,3 +19,31 @@ $(document).ready(function() {
 	});
 
 });
+
+function orientTextBoxes(messageArea) {
+
+	var messages = messageArea.children;
+	
+
+	if (messages.length > 1) {
+		var currentSide = messages[messages.length - 1].classList.contains("left") ? "left" : "right";
+
+		/** If at least two messages stacked, style them accordingly **/
+		if (messages[messages.length -2].classList.contains(currentSide)) {
+
+			if (messages[messages.length -2].classList.contains("bottom")) {
+				messages[messages.length -2].classList.remove("bottom");
+				messages[messages.length -2].classList.add("mid");
+				messages[messages.length -1].classList.add("bottom");
+			}
+
+			else {
+				messages[messages.length -2].classList.add("top");
+				messages[messages.length -1].classList.add("bottom");
+			}
+
+		}
+
+	}
+
+}
